354ffd6f8a1c17dbca68348797c6ed4c3a447acc
임재환 <jh.lim@piolink.com>
2026-02-12 09:20:57 +0900

    convb int formater

--- devcli/cmd/convb.go
+++ devcli/cmd/convb.go
@@ -4,6 +4,7 @@ package cmd
 import (
     "os"
     "fmt"
+    "strconv"
 
     "github.com/spf13/cobra"
 )
@@ -17,7 +18,35 @@ func stringToBinary(data string) ([]byte, error) {
 
 
 func intToBinary(data string) ([]byte, error) {
-	return nil, nil
+
+    // SIG: func ParseInt(s string, base int, bitSize int) (i int64, err error)
+    // if base is 0, the true base is implied by the string's prefix following
+    // the sign 0b, 0x 0o, 0.
+    int_data, err := strconv.ParseInt(data, 0, 64);
+    if err != nil {
+        return nil, err;
+    }
+
+    return uint64toBinary(uint64(int_data));
+}
+
+
+func uint64toBinary(data uint64) ([]byte, error) {
+    // 8 bits * 8 = 64 bits
+    buf := make([]byte, 8)
+    for i := 0; i < 8; i++ {
+        var currentByte byte = 0
+        for j := 0; j < 8; j++ {
+            currentByte <<= 1;
+            // 63: uint64 에서 최상위 비트를 취득하기 위함.
+            if (data & (1 << 63)) != 0 {
+                currentByte |= 1;
+            }
+            data <<= 1;
+        }
+        buf[i] = currentByte;
+    }
+    return buf, nil
 }
 
 
@@ -28,7 +57,7 @@ var ConvbCmd = &cobra.Command{
 	Run:   func(cmd *cobra.Command, args []string) {
 		formats := map[string]FormaterP{
 			"string": stringToBinary,
-			//"int": intToBinary,
+			"int": intToBinary,
 		}
 
 		// Get the format flag
@@ -48,9 +77,8 @@ var ConvbCmd = &cobra.Command{
 		// Convert to binary.
 		data := args[0]
 		bytes, err := formater(data)
-		fmt.Printf("%s\n", bytes);
 		for _, b := range bytes {
-			fmt.Printf("bytes:%08b\n", b);
+			fmt.Printf("byte:%08b\n", b);
 		}
 	},
 }
